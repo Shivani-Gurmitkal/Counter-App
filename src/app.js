67f82b014049f754378ee74a7f2993f8ace50c03
Shivani-Gurmitkal <shivanigurmitkal@gmail.com>
2025-03-28 19:29:03 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,23 +1,31 @@
 let count = 0;
 let counter = document.getElementById('counter');
 let btnIncrement = document.getElementById('btnIncrement');
-let btnDecement = document.getElementById('btnDecement');
+let btnDecrement = document.getElementById('btnDecrement');
+let btnReset = document.getElementById('btnReset');
 
 function increment(){
-    count = count+1;
+    count++;
     counter.innerText = count;
     btnIncrement.style.backgroundColor = 'green';
-    btnDecement.style.backgroundColor = 'black';
+    btnDecrement.style.backgroundColor = 'black';
 }
 
 function decrement(){
     if(count > 0){
-        count = count-1;
+        count--;
         counter.innerText = count;
-        btnDecement.style.backgroundColor = 'red';
+        btnDecrement.style.backgroundColor = 'red';
         btnIncrement.style.backgroundColor = 'black';
     }
     else{
-        btnDecement.style.backgroundColor = 'black';
+        btnDecrement.style.backgroundColor = 'black';
     }
+}
+
+function resetbutton(){
+    count = 0; 
+    counter.innerText = count;
+    btnIncrement.style.backgroundColor = 'black';
+    btnDecrement.style.backgroundColor = 'black';
 }
